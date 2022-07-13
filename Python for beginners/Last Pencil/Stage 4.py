21b7de2a26684d888c03e0e5c1fe45d034475466
Suraj Reddy <82399778+surajreddy72@users.noreply.github.com>
2022-07-13 07:15:26 +0530

    Update Stage 4.py

--- Python for beginners/Last Pencil/Stage 4.py
+++ Python for beginners/Last Pencil/Stage 4.py
@@ -3,46 +3,51 @@ def printpencils(n):
 
 def printturn(Name):
     print(Name + "'s turn!")
-
-
-n = input("How many pencils would you like to use:\n")
-
-while n.isnumeric() != True :
+    
+def order(Name):
+    if Name == "John" :
+        i = 1
+    elif Name == "Jack" :
+        i = 0
+    else :
+        while (Name != "John") or (Name != "Jack") :
+            print("Choose between 'John' and 'Jack'")
+            Name = input()
+            if Name == "John" :
+                i = 1
+                break
+            elif Name == "Jack" :
+                i = 0
+                break
+    return i
+    
+def checkpencils(n):
+    while n.isnumeric() != True :
         print("The number of pencils should be numeric")
         n = input()
-while int(n) <= 0 :
+    while int(n) <= 0 :
         print("The number of pencils should be positive")
         n = input()
         while n.isnumeric() != True :
             print("The number of pencils should be numeric")
             n = input()
-
-Name = input("Who will be the first (John, Jack):\n")
-if Name == "John" :
-        i = 1
-elif Name == "Jack" :
-        i = 0
-else :
-    while (Name != "John") or (Name != "Jack") :
-        print("Choose between 'John' and 'Jack'")
-        Name = input()
-        if Name == "John" :
-            i = 1
-            break
-        elif Name == "Jack" :
-            i = 0
-            break
-   
-
-while int(n) > 0:
-    printpencils(int(n))
-    if i % 2 == 1:
-        printturn("John")
-        i = i + 1
-    else :
-        printturn("Jack")
-        i = i + 1
-    m = input()
+    return n
+    
+def Game(n,i):
+    while int(n) > 0:
+        printpencils(int(n))
+        if i % 2 == 1:
+            printturn("John")
+            i = i + 1
+        else :
+            printturn("Jack")
+            i = i + 1
+        m = input()
+        m = checkinput(n,m)
+        n = int(n)-int(m)
+    return i
+    
+def checkinput(n,m):
     while m.isnumeric() != True :
         print("Possible values: '1', '2' or '3'")
         m = input()
@@ -55,9 +60,18 @@ while int(n) > 0:
     while int(m) > int(n):
         print("Too many pencils were taken")
         m = input()
-    n = int(n)-int(m)
-
-if i % 2 == 1:
+    return m
+    
+def Win(i):
+    if i % 2 == 1:
         print("John Won!")
-else :
-    print("Jack Won!")
+    else :
+        print("Jack Won!")
+
+    
+    
+n = input("How many pencils would you like to use:\n")
+n = checkpencils(n)
+Name = input("Who will be the first (John, Jack):\n")
+i = order(Name)
+Win(Game(n,i))
